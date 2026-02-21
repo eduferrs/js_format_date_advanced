c277ebed2000c90d6fb457476fae1a18929ab893
eduferrs <edu.ferr96@gmail.com>
2026-02-21 14:43:38 -0300

    done

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,36 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const oldIndexY = fromFormat.indexOf('YYYY');
+  const oldIndexM = fromFormat.indexOf('MM');
+  const oldIndexD = fromFormat.indexOf('DD');
+  const newIndexY = toFormat.indexOf('YYYY');
+  const newIndexM = toFormat.indexOf('MM');
+  const newIndexD = toFormat.indexOf('DD');
+  const newDate = [0, 1, 2];
+
+  const parts = date.split(fromFormat[3]);
+
+  if (newIndexY === -1) {
+    newDate[toFormat.indexOf('YY')] = parts[oldIndexY].slice(2);
+  } else {
+    newDate[newIndexY] = parts[oldIndexY];
+  }
+
+  if (oldIndexY === -1) {
+    if (parts[fromFormat.indexOf('YY')] < 30) {
+      newDate[newIndexY] = `20${parts[fromFormat.indexOf('YY')]}`;
+    } else {
+      newDate[newIndexY] = `19${parts[fromFormat.indexOf('YY')]}`;
+    }
+  } else {
+    newDate[newIndexY] = parts[oldIndexY];
+  }
+
+  newDate[newIndexM] = parts[oldIndexM];
+  newDate[newIndexD] = parts[oldIndexD];
+
+  return newDate.join(toFormat[3]);
 }
 
 module.exports = formatDate;
